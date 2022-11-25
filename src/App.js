dbd32b99dddf914011e1044945678e267374f8bf
olamideakintayo <divineolamide977@gmail.com>
2022-11-25 19:17:41 +0100

    json file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
-function App() {
-  return <div></div>;
+import data from "./json/Data";
+const App = () => {
+  const { items } = data;
+  return (
+  <div>
+    App
+  </div>
+  );
 }
 
 export default App;
---
+++ src/json/Data.js
@@ -0,0 +1,73 @@
+const data = {
+    
+        items: [
+          {
+               id: "1",
+               title: "queen panel bed",
+               price: 10.99,
+               image: "../images/product-1.jpeg",
+              inStock: true
+          },
+
+          {
+               id: "2",
+               title: "king panel bed",
+               price: 12.99,
+               image:  "./images/product-2.jpeg",
+               inStock: true
+            },
+        
+          {
+             id: "3",
+             title: "single panel bed",
+             price: 12.99,
+             image: "./images/product-3.jpeg",
+             soldout: true
+            },
+       
+          {
+              id: "4",
+              title: "twin panel bed",
+              price: 22.99,
+              image: "./images/product-4.jpeg",
+              inStock: true
+            },
+        
+          {
+            id: "5",
+            title: "fridge",
+            price: 88.99,
+            image: "./images/product-5.jpeg",
+            inStock: true
+            },
+         
+          {
+             id: "6",
+            title: "dresser",
+            price: 32.99,
+            image:  "./images/product-6.jpeg",
+            soldout: true
+            },
+        
+          {
+             id: "7",
+             title: "couch",
+             price: 45.99,
+            image: "./images/product-7.jpeg",
+            inStock: true
+            },
+        
+          {
+             id: "8",
+             title: "table",
+             price: 33.99,
+            image: { "fields": { "file": { "url": "./images/product-8.jpeg" } } },
+            inStock: true
+            },
+        
+        ]
+      }
+      
+
+
+export default data;
