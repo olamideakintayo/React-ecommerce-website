24eb661d95d0f6a40144d13d1dc899d33b6d411a
olamideakintayo <divineolamide977@gmail.com>
2022-11-26 13:00:05 +0100

    Hero Image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,12 +1,14 @@
+import Herosvg from '../styles/Hero-image.css'
 const Hero = () => {
     return (
         <div>
              {/*  Hero Section */}
-       <header class="hero flex justify-center items-center">
-      <div class="banner inline-block text-center animate__animated animate__bounceInLeft">
-      <h1 class="banner-title  flex font-body uppercase text-black">Kash Collections</h1>
+            
+       <header className="hero  flex justify-center items-center">
+      <div className="banner inline-block text-center animate__animated animate__bounceInLeft">
+      <h1 className="banner-title  flex font-body uppercase text-black">Kash Collections</h1>
       <a href="#product">
-      <button class="banner-btn uppercase cursor-pointer font-body  bg-brightRed">Shop Now</button> 
+      <button className="banner-btn uppercase cursor-pointer font-body  bg-brightRed">Shop Now</button> 
   </a>
    </div>
   </header>
