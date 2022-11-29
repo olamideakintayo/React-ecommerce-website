a5c0b072b6cd461f3e926bb9ff9c0a49a7cce742
olamideakintayo <divineolamide977@gmail.com>
2022-11-29 17:23:36 +0100

    products image

--- src/products/Products.js
+++ src/products/Products.js
@@ -5,10 +5,16 @@ const Products = ({items}) => {
     console.log(items)
     return (
         <div className="">
-            {/* We then create a map function that whenever we call it, it will create a new array from the existing array and display the items */ }
-            {items.map((item) =>(
+             <section class="products font-body" id="product">
+    <div class="section-title text-center capitalize">
+      <h2 class="text-center capitalize animate__animated animate__flipInX animate__delay-2s animte__duration-3">our products</h2>
+    </div>
+   
+    <div class="products-center grid m-auto">
+{/* We then create a map function that whenever we call it, it will create a new array from the existing array and display the items */ }
+{items.map((item) =>(
                 <div className=""> 
-                    <div className="">
+                       <div className="">
                         <img className="" src={item.image} alt={item.title} />
                     </div>
                     <div>
@@ -19,6 +25,10 @@ const Products = ({items}) => {
                 </div>
             ))}
         </div>
+        </section>
+
+</div>
+            
     );
 };
 export default Products;
